be239c5cc8d4c81a8298aecaeba42fdb63a60da1
Nasim Alam <nasimal786@gmail.com>
2023-09-24 11:58:25 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef } from 'react'
+import {Bar, getElementAtEvent} from "react-chartjs-2"
+import { Chart as chartjs,BarElement,CategoryScale,LinearScale,Tooltip,Legend } from 'chart.js'
+chartjs.register(BarElement,CategoryScale,LinearScale,Tooltip,Legend)
 function App() {
+const data = {
+  labels: ['mon','tue','wed','thu','fri','sat'],
+  datasets : [{
+    label: 'sales details',
+    data: [33,24,55,63,99,65],
+    backgroundColor: ['red','blue'],
+    borderColor: 'black',
+    borderWidth: 1
+  }]
+}
+const options = {}
+const chartRef=useRef();
+function handleClick(event){
+  if(getElementAtEvent(chartRef.current,event).length >0){
+    console.log(getElementAtEvent(chartRef.current,event))
+  }
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: '600px',height: '60%'}}>
+      <Bar data={data} ref={chartRef} options={options} onClick={handleClick} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
